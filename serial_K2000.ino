9ea7fbf2c752e0cb14247c374932b62d4d4844a2
iidre-eeepc <iidre-eeepc@asus-eeepc.(none)>
2014-01-15 02:42:07 +0100

    Encapsulate in void param_serial

--- serial_K2000.ino
+++ serial_K2000.ino
@@ -1,5 +1,5 @@
 /*
-  
+  Modified version of an Arduino example:  
 
   Reading a serial ASCII-encoded string.
   created 13 Apr 2012
@@ -9,12 +9,23 @@
  */
 
 void setup() {
-  // initialize serial:
-  Serial.begin(9600);
+  setup_param_serial()
 }
 
 void loop() {
   ///////  ReadASCIIString ///////
+  param_serial()
+    
+  ///////  Motor Control  ///////
+  motor_control_refresh()
+}
+
+void setup_param_serial(){
+  // initialize serial:
+  Serial.begin(9600);
+}
+
+void param_serial(){
   // if there's any serial available, read it:
   while (Serial.available() > 0) {
 
@@ -29,15 +40,12 @@ void loop() {
     if (Serial.read() == '\n') {
       param_update(param_id, param_value)
     }
-    
-  ///////  Motor Control  ///////
-  // TODO:
   }
 }
-
-
 
 void param_update(param_id, param_value){
 // TODO:
 }
-
+void  motor_control_refresh(){
+// TODO:
+}
